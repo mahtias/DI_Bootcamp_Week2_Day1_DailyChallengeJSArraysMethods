db468b4c4ea4b99127842b779ac407781206936c
mahtias <52318230+mahtias@users.noreply.github.com>
2023-01-31 16:23:26 +0000

    first depot

---
+++ js/script.js
@@ -0,0 +1,32 @@
+
+
+// methode shift supression d'une shaine de caractere dans un tableau
+
+
+
+const fruits = ["Banana", "Apples", "Oranges", "Blueberries"];
+
+fruits.shift(0) // shift supression d'une shaine de caractere
+
+fruits.sort() // Triez le tableau par ordre alphabétique.
+
+fruits.push("Kiw") //ajouter  un caracter dans le tableau
+
+fruits.splice() //suprimer une chaine de caractere dans un tableau
+
+// pour inverser les caractere dans un tavleau
+
+console.log(fruits.reverse())
+
+// Exercice
+
+const moreFruits = ["Banana", ["Apples", ["Oranges"], "Blueberries"]];
+
+console.log(moreFruits [1] [1]);
+
+
+
+
+
+
+
